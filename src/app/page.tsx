47d8bbc5c5b6c6d285a86cb693f40b972e7a07d2
shashipoojary <shashikanthpoojary61@gmail.com>
2025-09-15 17:04:37 +0530

    Make logo even bigger for better prominence
    - Mobile: w-20 h-20  w-24 h-24 (80px  96px)
    - Small screens: w-24 h-24  w-28 h-28 (96px  112px)
    - Medium screens: w-28 h-28  w-32 h-32 (112px  128px)
    - Large screens: w-32 h-32  w-36 h-36 (128px  144px)
    - Extra large: w-36 h-36  w-40 h-40 (144px  160px)
    - Logo now has more visual impact and better brand presence

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -342,7 +342,7 @@ InvoiceFlow Team`;
         alt="InvoiceFlow Logo" 
         width={160}
         height={160}
-        className="w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 lg:w-32 lg:h-32 xl:w-36 xl:h-36"
+        className="w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 lg:w-36 lg:h-36 xl:w-40 xl:h-40"
       />
     </div>
   );
